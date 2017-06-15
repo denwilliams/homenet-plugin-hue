57fe70c66df251c4cd630b80dbe469d71cdaeeb8
Den Williams <work@denwilliams.net>
2017-06-15 19:00:30 +1000

    use new plugin load method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export { HuePluginLoader } from './loader';
+import { create } from './loader';
+
+function load(annotate): any {
+  return create(annotate);
+};
+
+export = load;
--- src/loader.ts
+++ src/loader.ts
@@ -1,29 +1,35 @@
 /// <reference path="./hue-api.d.ts"/>
 
-import {plugin, service, IPluginLoader, ILogger, IConfig, ILightsManager, ISettable, IClassTypeFactory} from '@homenet/core';
+import {IPluginLoader, ILogger, IConfig, ILightsManager, ISettable, IClassTypeFactory} from '@homenet/core';
 
 import { HueController } from './hue-controller';
 import { HueLight } from './hue-light';
 
-@plugin()
-export class HuePluginLoader implements IPluginLoader {
-  private controller : HueController;
+export function create(annotate): { HuePluginLoader: new(...args: any[]) => IPluginLoader } {
+  @annotate.plugin()
+  class HuePluginLoader implements IPluginLoader {
+    private controller : HueController;
 
-  constructor(
-          @service('IConfig') private config: IConfig,
-          @service('ILightsManager') private lights: ILightsManager,
-          @service('ILogger') private logger: ILogger) {
-    this.controller = new HueController(config, logger);
-    const lightFactory: IClassTypeFactory<ISettable> = this.hueLightFactory.bind(this);
-    lights.addSettableType('hue', lightFactory);
+    constructor(
+            @annotate.service('IConfig') private config: IConfig,
+            @annotate.service('ILightsManager') private lights: ILightsManager,
+            @annotate.service('ILogger') private logger: ILogger) {
+      this.controller = new HueController(config, logger);
+      const lightFactory: IClassTypeFactory<ISettable> = this.hueLightFactory.bind(this);
+      lights.addSettableType('hue', lightFactory);
+    }
+
+    load() : void {
+      this.logger.info('Loading hue lights');
+    }
+
+    private hueLightFactory(id : string, opts : any) : ISettable {
+      this.logger.info('Adding Hue light: ' + id);
+      return new HueLight(id, opts, this.controller, this.logger);
+    }
   }
 
-  load() : void {
-    this.logger.info('Loading hue lights');
-  }
-
-  private hueLightFactory(id : string, opts : any) : ISettable {
-    this.logger.info('Adding Hue light: ' + id);
-    return new HueLight(id, opts, this.controller, this.logger);
-  }
-}
+  return {
+    HuePluginLoader
+  };
+};
